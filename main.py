fda57b400e17f9dbf65daca933dcd277ac3865f5
hemvall <louisserrano777@gmail.com>
2025-02-12 21:19:01 +0100

    Push Navokado Bot

---
+++ main.py
@@ -0,0 +1,64 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackContext, Updater
+
+TOKEN: Final = '7548797793:AAEpmkfJmu7XXvuDVQIrBTy0eC_i51PQxlM'
+BOT_USERNAME = '@NavokadoBot'
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello, pleased to meet you!')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('I am Navokado, Please type something so I can respond')
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('This is a custom command.')
+
+# Responses
+def handle_response(text: str)->str:
+    processed: str = text.lower()
+    if 'hello' in processed:
+        return 'Hey there!'
+    if 'hey' in processed:
+        return 'Hello there!'
+    if 'avokado' in processed:
+        return 'I love avokados!'
+    return 'I do not understand..'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User ({update.message.chat.id}) type: {message_type}: "{text}"')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+    print('Starting telegram bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', start_command))
+
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Errors
+    app.add_error_handler(error)
+    print('Polling telegram bot...')
+    app.run_polling(poll_interval=3)
